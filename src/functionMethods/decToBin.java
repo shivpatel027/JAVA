1cd92288c60aa55ca85ad297765391580bbcf384
shivpatel027 <patelshiv1701@gmail.com>
2024-06-18 06:50:08 -0500

    Arrays started

--- src/functionMethods/decToBin.java
+++ src/functionMethods/decToBin.java
@@ -5,7 +5,7 @@ public class decToBin {
         int num = n;
         int pow = 0;
         int binNum = 0;
-
+ 
         while(n>0) {
             int rem = n % 2;
             binNum = binNum + (rem * (int)Math.pow(10, pow));
