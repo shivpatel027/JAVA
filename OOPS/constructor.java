d9a03f3bc5eb8d853c5a2006aa789e774e7e6963
shivpatel027 <patelshiv1701@gmail.com>
2024-07-08 19:19:12 -0500

    Types of Constructor

--- OOPS/constructor.java
+++ OOPS/constructor.java
@@ -3,7 +3,8 @@ package OOPS;
 public class constructor {
     public static void main(String[] args) {
         Student student = new Student();
-
+        Student s2 = new Student("Shiv");
+        Student s3 = new Student(123);
     }
 }
 
@@ -13,5 +14,13 @@ class Student {
 
     Student() {
         System.out.println("Constructor is called......");
+    }
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    Student(int roll) {
+        this.roll = roll;
     }
 }
